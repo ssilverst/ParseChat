71736f417e11b7e2d6012e0bbe1606a43cdc34c4
Samantha (sam) Silverstein <selinons@fb.com>
2019-07-07 10:09:14 -0700

    cleaning up files

---
+++ LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  ParseChat
+//
+//  Created by selinons on 7/7/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
